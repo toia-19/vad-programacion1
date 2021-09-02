f610bbf4c1343e6859acb69b8744d2e0dae94063
toia-19 <vickingiediaz19@gmail.com>
2021-09-02 16:59:41 -0300

    POO - Bhaskara

--- poo-ejercicio1/bhaskara.js
+++ poo-ejercicio1/bhaskara.js
@@ -14,13 +14,13 @@ export default class Bhaskara {
         const bloque1 = Math.pow(this.cb,2) - (4*this.ca*this.ti);
         const raiz = Math.sqrt(bloque1);
         const r1 = (-this.cb + raiz) / (this.ca*2);
-        return r1;
+        return r1.toFixed(2);
     }
 
     calcular_r2(){
         const bloque1 = Math.pow(this.cb,2) - (4*this.ca*this.ti);
         const raiz = Math.sqrt(bloque1);
         const r2 = (-this.cb - raiz) / (this.ca*2);
-        return r2;
+        return r2.toFixed(2);
     }
 }
